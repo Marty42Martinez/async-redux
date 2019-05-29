faa7e37f87f881830ebdf7c5b2053c07309d90e4
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-05-29 14:23:47 -0700

    goofin around with middleware

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './components/App';
+// import App from './components/App';
+import { createStore, applyMiddleware } from 'redux';
 
-render(
-  <App />,
-  document.getElementById('root')
+const logger = store => next => action => {
+  console.log('ACTION RECEIVED::', action.type);
+  next(action);
+};
+
+
+function reducer(state = {}, action) {
+  switch(action.type) {
+    case 'TALKING':
+      return { ...state, [action.type]: [action.payload] };
+  }
+}
+
+
+const store = createStore(
+  reducer,
+  applyMiddleware(logger)
 );
+
+store.dispatch({
+  type: 'TALKING',
+  payload: 'Not sure what to do'
+});
