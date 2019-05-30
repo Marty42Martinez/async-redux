4eecc7d149e660eac6179b06ff65454a7621ef21
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-05-30 10:12:59 -0700

    case2, cleaned up initState

--- src/reducers/charactersReducer.js
+++ src/reducers/charactersReducer.js
@@ -1,12 +1,16 @@
 import { FETCH_CHARACTERS_LOADING, FETCH_CHARACTERS, FETCH_CHARACTERS_ERROR } from '../actions/charactersActions';
 
-cons initialState = {
-
+const initialState = {
+  loading: false,
+  list: [],
+  error: null
 };
 
 export default function reducer(state = initialState, action) {
   switch(action.type) {
     case FETCH_CHARACTERS_LOADING:
       return { ...state, loading: true };
+    case FETCH_CHARACTERS:
+      return { ...state, error: null, loading: false, list: action.payload };
   }
 }
