bbfc64f07efa0b3eeca36bae3faca75f6a7fba3e
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-05-30 17:47:04 -0700

    called the proper comps in app

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import CharacterQuotes from '../containers/CharacterQuotes';
 
 export default function App() {
   return (
-    <h1>Hello Dev!</h1>
+    <CharacterQuotes />
   );
 }
